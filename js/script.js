88c5f3b513bfc8f5ddc4222705104709b6d3b794
Phani Allamsetty <phani.allamsetty@gmail.com>
2022-03-03 16:20:28 -0500

    Added the start time for engagementDisplayStart in loginClick

--- js/script.js
+++ js/script.js
@@ -91,6 +91,7 @@ function appendEventToLog(eventName) {
 
 function loginClick(event) {
 	event.preventDefault();
+	window.engagementDisplayStart = new Date();
 	const siteId = document.getElementById('siteId').value;
 	const username = document.getElementById('username').value;
 
