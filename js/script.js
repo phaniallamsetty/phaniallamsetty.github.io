32c4a31a2b989b7339eb078032862a60f33e4bea
Phani Allamsetty <phani.allamsetty@gmail.com>
2022-03-04 15:36:48 -0500

    Testing cookies

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ var engagementDisplayStart;
 var engagementDisplayEnd;
 var messagingWindowLoadStart;
 var messagingWindowLoadEnd;
+var customDimensions = {};
 
-bindToEvents();
+init();
+
+function init() {
+	bindToEvents();
+	setCustomDimensions();
+}
 
 function bindToEvents() {
 	events.bind({
@@ -87,6 +93,24 @@ function appendEventToLog(eventName) {
     var elem = document.getElementById('data-layer-log');
     elem.innerHTML += '<br>';
     elem.innerHTML += '<code>' + eventName + '</code>';
+}
+
+function setCustomDimensions() {
+	var cookieArray = [];
+
+	if(document.cookie && document.cookie.length > 0) {
+		cookieArray = document.cookie.split(';');
+
+		cookieArray.forEach(function(x) {
+			console.log('x', x);
+		})
+	}
+
+	// customDimensions = {
+	// 	isNewUser: getIsNewUserVal(),
+	// 	lpVisitorId: getVisitorId(),
+	// 	lpSessionId: getLpSessionId()
+	// }
 }
 
 function loginClick(event) {
